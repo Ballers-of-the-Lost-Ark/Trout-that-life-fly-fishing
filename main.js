a8306149fc07bc98f2549e033c3bc3a2b01b083d
Nick Zinn <nzinn43@gmail.com>
2019-02-05 11:45:43 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,70 @@
+// Add "a" tag to packages
+const packageEffect = document.querySelector('.package1');
+const packageEffect2 = document.querySelector('.package2');
+const packageEffect3 = document.querySelector('.package3');
+
+packageEffect.addEventListener("click", onClick);
+function onClick(){
+    window.open("#", "_blank");
+}
+
+packageEffect2.addEventListener("click", onClick);
+function onClick(){
+    window.open("#", "_blank");
+}
+
+packageEffect3.addEventListener("click", onClick);
+function onClick(){
+    window.open("#", "_blank");
+}
+
+// adding a tags to logo
+const nav  = document.getElementById('logo');
+nav.addEventListener('click', logoClick);
+function logoClick(){
+    window.open("#", "_blank");
+}
+
+// Animated nav slider
+if(window.screen.width < 890){
+  const menu = document.createElement('i');
+  menu.className ="fas fa-bars";
+
+  const wrapper = document.querySelector('.wrapper');
+  const header = document.querySelector('.header');
+  wrapper.insertBefore(menu, header);
+
+  let on = false;
+  menu.addEventListener('click', () => {
+  if(on === false){
+    menu.classList.remove('grow');
+    header.classList.remove('transition-off');
+
+    header.classList.toggle('transition-on');
+    menu.classList.toggle('shrink');
+
+    on = true;
+  }else{
+    header.classList.remove('transition-on');
+    menu.classList.remove('shrink');
+
+    menu.classList.toggle('grow');
+    header.classList.toggle('transition-off');
+   
+    on = false;
+  }
+
+  });
+
+  function shrinkOnClick(){
+    setTimeout(() => {
+      header.classList.remove('transition-on');
+      menu.classList.remove('shrink');
+  
+      menu.classList.toggle('grow');
+      header.classList.toggle('transition-off');
+     
+      on = false;
+    }, 900);
+  }
+}
